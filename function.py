19d34b920e8ec658ed488dcb656102a2f1c58fa1
HangbaSteve <helllll@foxmail.com>
2023-06-09 23:55:39 +0800

    Optimised test functions.

--- function.py
+++ function.py
@@ -1,13 +1,14 @@
 import requests
+import json
 from pythonping import ping
 
 
-def ping_host(ThreadName, msg):
+def ping_host(*args):
     
     ping_result = ping(target="www.nnzkzs.com", count=10, timeout=2)
 
-    msg[0]= f"""{ThreadName}
-    Average Latency: {ping_result.rtt_avg_ms}ms,
+    return \
+    f"""Average Latency: {ping_result.rtt_avg_ms}ms,
     Minimal Latency: {ping_result.rtt_min_ms}ms,
     Maximal Latency: {ping_result.rtt_max_ms}ms,
     Packet Loss: {ping_result.packet_loss}%
@@ -15,14 +16,40 @@ def ping_host(ThreadName, msg):
 
 def post(schoolCode, status):
     types = ["instruction","directional","guide"]
-    js = []
+    isInfoAPISuccessful = []
     for t in types:
         d = {'schoolCode':schoolCode,"type":t,"status":status}
         url="http://www.nnzkzs.com/api/services/app/publicityDetail/GetGeneralDetail?schoolCode=%s&type=%s&status=4"%(str(schoolCode),t)
-        res=requests.post(url,data=d)
-        js.append(res.json())
-    with open("test.txt","w") as f:
-        f.write(str(js))
+        #Test for API which is used to get detailed registeration data
+        try:
+            res=requests.post(url,data=d)
+            isInfoAPISuccessful.append(res.json()["success"])
+        except:
+            isInfoAPISuccessful.append(False)
+    info_available = all(isInfoAPISuccessful)
+
+    url2 = "http://www.nnzkzs.com/api/services/app/generalPublicity/GetPublicity"
+    #Test for API which is used to get general school information
+    try:
+        res=requests.post(url2)
+        general_list_available = res.json()['success']
+    except:
+        general_list_available = False
+
+    url3 = "http://www.nnzkzs.com/api/services/app/vocationalPublicity/GetPublicity"
+    #Test for API which is used to get vocational school information
+    try:
+        res=requests.post(url3)
+        vocational_list_available = res.json()['success']
+    except Exception as e:
+        vocational_list_available = False
+
+    return \
+    f"""Registeration Data API Availablity: {str(info_available)}
+    General School Information API Availablity: {str(general_list_available)}
+    Vocational School Information API Availablity: {str(vocational_list_available)}"""
+
+    
 
 
 def initialize():
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore, QtGui, uic, QtWidgets
+from PyQt5.QtCore import QObject,pyqtSignal,QThread
 from threading import Thread
 from function import *
 import sys
@@ -17,36 +18,73 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.Menu_Online_Ping.triggered.connect(self.ping_thread)
         #ping action
-        self.Menu_Online_Send.triggered.connect(self.post_thread)
+        self.Menu_Online_Post.triggered.connect(self.post_thread)
 
     def post_thread(self):
-        try:
-            thread2 = Thread(target=post,args=(2002,self.status))
-            thread2.start()
-        except Exception as e:
-            msg = [f"Network Error: {str(e)}"]
+        self.statusbar.showMessage("Sending POST method to API.")
+        self.status = self.StatusNum.toPlainText() # refresh status number
+        self.thread2 = QThread()
+        self.worker2 = Worker(post,2002,self.status,title = "API Availablity")
+        # Move the worker to the thread
+        self.worker2.moveToThread(self.thread2)
+        # Connect signals and slots
+        self.thread2.started.connect(self.worker2.run)
+        self.worker2.finished.connect(self.thread2.quit)
+        self.worker2.finished.connect(self.worker2.deleteLater)
+        self.thread2.finished.connect(self.thread2.deleteLater)
+        self.worker2.result.connect(lambda *args: self.information_box(*args))
+        # Start the thread
+        self.thread2.start()
+
 
 
     def ping_thread(self):
         self.statusbar.showMessage("Pinging www.nnzkzs.com.")
-        #ping action layer
-        msg = [None]
-        try:
-            thread1 = Thread(target=ping_host,args=("Ping www.nnzkzs.com",msg))
-            thread1.start()
-            thread1.join()
-        except Exception as e:
-            msg = [f"Network Error: {str(e)}"]
+        # Create a QThread object and a worker object
+        self.thread1 = QThread()
+        self.worker1 = Worker(ping_host, title = "Pinging Result")
+        # Move the worker to the thread
+        self.worker1.moveToThread(self.thread1)
+        # Connect signals and slots
+        self.thread1.started.connect(self.worker1.run)
+        self.worker1.finished.connect(self.thread1.quit)
+        self.worker1.finished.connect(self.statusbar.showMessage)
+        self.worker1.finished.connect(self.worker1.deleteLater)
+        self.thread1.finished.connect(self.thread1.deleteLater)
+        self.worker1.result.connect(lambda *args: self.information_box(*args))
+        # Start the thread
+        self.thread1.start()
+        
+        
+    def information_box(self, information, title = "Information"):
         box = QtWidgets.QMessageBox()
-        box.setText(msg[0])
-        #TODO: Optimize the function of getting return value from thread
-        box.setWindowTitle("Ping Information")
+        box.setText(information)
+        box.setWindowTitle(title)
         box.setIcon(QtWidgets.QMessageBox.Information)
         box.setStandardButtons(QtWidgets.QMessageBox.Ok)
         box.exec_()
-        self.statusbar.showMessage("Finished!")
-        
 
+class Worker(QObject):
+    # define a signal that will emit the result
+    result = pyqtSignal(object,object)
+    finished = pyqtSignal(object)
+
+    def __init__(self,target,*args, title = "Information"):
+        super().__init__()
+        self.title = title
+        self.target = target
+        self.args = args
+
+    def run(self):
+        # perform the task and emit the result
+        try:
+            msg = self.target(*self.args)
+            self.result.emit(msg,self.title)
+        except Exception as e:
+            msg = f"Error: {str(e)}"
+            self.result.emit(msg,self.title)
+        # Emit the finished signal
+        self.finished.emit("Finish.")
 
 app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
